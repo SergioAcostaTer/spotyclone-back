c9a8acddb9de5e83eea225fc347ed1fe03bafee1
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-31 22:53:40 +0100

    prooving with nothin

--- routes/spotify.js
+++ routes/spotify.js
@@ -1,97 +1,97 @@
-const express = require("express");
-const { searchSpotify, mmssToSeconds } = require("../utils/spotify");
-const { getColorFromURL } = require("color-thief-node");
+// const express = require("express");
+// const { searchSpotify, mmssToSeconds } = require("../utils/spotify");
+// const { getColorFromURL } = require("color-thief-node");
 
-const router = express.Router();
+// const router = express.Router();
 
-require("dotenv").config();
-router.get("/search/:query", async (req, res) => {
-  const { query } = req.params;
+// require("dotenv").config();
+// router.get("/search/:query", async (req, res) => {
+//   const { query } = req.params;
 
-  try {
-    const spotifyResults = await searchSpotify(query);
+//   try {
+//     const spotifyResults = await searchSpotify(query);
 
-    const formatedSpotifyResults = await Promise.all(
-      spotifyResults.map(async (item) => {
-        const dominantColor = await getColorFromURL(item.album.images[0].url);
-        const color = dominantColor
-          .map((c) => c.toString(16).padStart(2, "0"))
-          .join("");
+//     const formatedSpotifyResults = await Promise.all(
+//       spotifyResults.map(async (item) => {
+//         const dominantColor = await getColorFromURL(item.album.images[0].url);
+//         const color = dominantColor
+//           .map((c) => c.toString(16).padStart(2, "0"))
+//           .join("");
 
-        return {
-          title: item.name,
-          artist: item.artists[0].name,
-          allArtists: item.artists.map((artist) => artist.name),
-          spotifyUrl: item.external_urls.spotify,
-          id: item.id,
-          thumbnail: item.album.images[0].url,
-          thumbnailSmall: item.album.images.slice(-1)[0]?.url,
-          duration: item.duration_ms,
-          popularity: item.popularity,
-          type: "track",
-          color: `#${color}`,
-        };
-      })
-    );
+//         return {
+//           title: item.name,
+//           artist: item.artists[0].name,
+//           allArtists: item.artists.map((artist) => artist.name),
+//           spotifyUrl: item.external_urls.spotify,
+//           id: item.id,
+//           thumbnail: item.album.images[0].url,
+//           thumbnailSmall: item.album.images.slice(-1)[0]?.url,
+//           duration: item.duration_ms,
+//           popularity: item.popularity,
+//           type: "track",
+//           color: `#${color}`,
+//         };
+//       })
+//     );
 
-    res.json(formatedSpotifyResults);
-  } catch (error) {
-    console.error("Error:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while processing your request." });
-  }
-});
+//     res.json(formatedSpotifyResults);
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while processing your request." });
+//   }
+// });
 
-// get yt link from spotify song
+// // get yt link from spotify song
 
-const yt = require("ytsr");
+// const yt = require("ytsr");
 
-router.get("/search/:title/:artist/:time", async (req, res) => {
-  const { title, artist, time } = req.params;
+// router.get("/search/:title/:artist/:time", async (req, res) => {
+//   const { title, artist, time } = req.params;
 
-  try {
-    // const filters = await yt.getFilters(`${title} ${artist}`);
-    console.log(`${title} ${artist}`)
-    // const filter = await filters.get("Type").get("Video");
+//   try {
+//     // const filters = await yt.getFilters(`${title} ${artist}`);
+//     console.log(`${title} ${artist}`)
+//     // const filter = await filters.get("Type").get("Video");
 
-    const searchOptions = {
-      limit: 10,
-    };
+//     const searchOptions = {
+//       limit: 10,
+//     };
 
-    const searchResults = await yt(`${title} ${artist}`, searchOptions);
+//     const searchResults = await yt(`${title} ${artist}`, searchOptions);
 
-    let bestResult = null;
-    let minTimeDiff = Infinity;
-    let duration = {};
+//     let bestResult = null;
+//     let minTimeDiff = Infinity;
+//     let duration = {};
 
-    for (const video of searchResults.items) {
-      const songDuration = time;
-      const videoDuration = mmssToSeconds(video.duration);
-      const timeDiff = Math.abs(videoDuration - songDuration);
+//     for (const video of searchResults.items) {
+//       const songDuration = time;
+//       const videoDuration = mmssToSeconds(video.duration);
+//       const timeDiff = Math.abs(videoDuration - songDuration);
 
-      if (timeDiff < minTimeDiff) {
-        minTimeDiff = timeDiff;
-        bestResult = video;
-        duration = {
-          youtube: videoDuration,
-          spotify: Number(songDuration),
-        };
-      }
-    }
+//       if (timeDiff < minTimeDiff) {
+//         minTimeDiff = timeDiff;
+//         bestResult = video;
+//         duration = {
+//           youtube: videoDuration,
+//           spotify: Number(songDuration),
+//         };
+//       }
+//     }
 
-    res.json({
-      title: bestResult.title,
-      url: bestResult.url,
-      views: bestResult.views,
-      duration,
-    });
-  } catch (error) {
-    console.error("Error:", error);
-    res
-      .status(500)
-      .json({ error: "An error occurred while processing your request." });
-  }
-});
+//     res.json({
+//       title: bestResult.title,
+//       url: bestResult.url,
+//       views: bestResult.views,
+//       duration,
+//     });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res
+//       .status(500)
+//       .json({ error: "An error occurred while processing your request." });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
--- utils/spotify.js
+++ utils/spotify.js
@@ -1,53 +1,53 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-require("dotenv").config()
+// require("dotenv").config()
 
-async function getSpotifyAccessToken() {
-  const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-  const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+// async function getSpotifyAccessToken() {
+//   const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+//   const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 
-  if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
-    console.error("Spotify client credentials are missing.");
-    process.exit(1);
-  }
+//   if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
+//     console.error("Spotify client credentials are missing.");
+//     process.exit(1);
+//   }
 
-  const auth = Buffer.from(
-    `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
-  ).toString("base64");
+//   const auth = Buffer.from(
+//     `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+//   ).toString("base64");
 
-  const response = await axios.post(
-    "https://accounts.spotify.com/api/token",
-    "grant_type=client_credentials",
-    {
-      headers: {
-        Authorization: `Basic ${auth}`,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    }
-  );
+//   const response = await axios.post(
+//     "https://accounts.spotify.com/api/token",
+//     "grant_type=client_credentials",
+//     {
+//       headers: {
+//         Authorization: `Basic ${auth}`,
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//     }
+//   );
 
-  return response.data.access_token;
-}
+//   return response.data.access_token;
+// }
 
-async function searchSpotify(query) {
-  const spotifyToken = await getSpotifyAccessToken();
+// async function searchSpotify(query) {
+//   const spotifyToken = await getSpotifyAccessToken();
 
-  const responseSearch = await axios.get(
-    `https://api.spotify.com/v1/search?q=${query}&type=track&limit=20&offset=0`,
-    {
-      headers: {
-        Authorization: `Bearer ${spotifyToken}`,
-      },
-    }
-  );
+//   const responseSearch = await axios.get(
+//     `https://api.spotify.com/v1/search?q=${query}&type=track&limit=20&offset=0`,
+//     {
+//       headers: {
+//         Authorization: `Bearer ${spotifyToken}`,
+//       },
+//     }
+//   );
 
-  return responseSearch.data.tracks.items;
-}
+//   return responseSearch.data.tracks.items;
+// }
 
-function mmssToSeconds(mmss) {
-  if (!mmss) return
-  const [minutes, seconds] = mmss.split(":").map(Number);
-  return minutes * 60 + seconds;
-}
+// function mmssToSeconds(mmss) {
+//   if (!mmss) return
+//   const [minutes, seconds] = mmss.split(":").map(Number);
+//   return minutes * 60 + seconds;
+// }
 
-module.exports = { getSpotifyAccessToken, searchSpotify, mmssToSeconds };
+// module.exports = { getSpotifyAccessToken, searchSpotify, mmssToSeconds };
