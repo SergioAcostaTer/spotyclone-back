8c991da8033b520a232988e4b1a50a42dc2319e4
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-31 15:13:42 +0100

    likes

--- index.js
+++ index.js
@@ -1,43 +1,105 @@
 require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
-// const { searchSpotify } = require("./utils/spotify");
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-// const search = require("./routes/spotify")
+const { searchSpotify, mmssToSeconds } = require("./utils/spotify");
+const { getColorFromURL } = require("color-thief-node");
 
-// app.get("/search/:query", async (req, res) => {
-//   const { query } = req.params;
 
-//   try {
-//     const spotifyResults = await searchSpotify(query);
 
-//     const formatedSpotifyResults = spotifyResults.map((item) => ({
-//       title: item.name,
-//       artist: item.artists[0].name,
-//       allArtists: item.artists.map((artist) => artist.name),
-//       spotifyUrl: item.external_urls.spotify,
-//       id: item.id,
-//       thumbnail: item.album.images[0].url,
-//       thumbnailSmall: item.album.images.slice(-1)[0]?.url,
-//       duration: item.duration_ms,
-//       popularity: item.popularity,
-//       type: "track",
-//     }));
-//     console.log(spotifyResults);
+app.get("/search/:query", async (req, res) => {
+  const { query } = req.params;
 
-//     res.json(formatedSpotifyResults);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res
-//       .status(500)
-//       .json({ error: "An error occurred while processing your request." });
-//   }
-// });
+  try {
+    const spotifyResults = await searchSpotify(query);
+
+    const formatedSpotifyResults = await Promise.all(
+      spotifyResults.map(async (item) => {
+        const dominantColor = await getColorFromURL(item.album.images[0].url);
+        const color = dominantColor
+          .map((c) => c.toString(16).padStart(2, "0"))
+          .join("");
+
+        return {
+          title: item.name,
+          artist: item.artists[0].name,
+          allArtists: item.artists.map((artist) => artist.name),
+          spotifyUrl: item.external_urls.spotify,
+          id: item.id,
+          thumbnail: item.album.images[0].url,
+          thumbnailSmall: item.album.images.slice(-1)[0]?.url,
+          duration: item.duration_ms,
+          popularity: item.popularity,
+          type: "track",
+          color: `#${color}`,
+        };
+      })
+    );
+
+    res.json(formatedSpotifyResults);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
+});
+
+
+const yt = require("ytsr");
+
+app.get("/search/:title/:artist/:time", async (req, res) => {
+  const { title, artist, time } = req.params;
+
+  try {
+    // const filters = await yt.getFilters(`${title} ${artist}`);
+    console.log(`${title} ${artist}`)
+    // const filter = await filters.get("Type").get("Video");
+
+    const searchOptions = {
+      limit: 10,
+    };
+
+    const searchResults = await yt(`${title} ${artist}`, searchOptions);
+
+    let bestResult = null;
+    let minTimeDiff = Infinity;
+    let duration = {};
+
+    for (const video of searchResults.items) {
+      const songDuration = time;
+      const videoDuration = mmssToSeconds(video.duration);
+      const timeDiff = Math.abs(videoDuration - songDuration);
+
+      if (timeDiff < minTimeDiff) {
+        minTimeDiff = timeDiff;
+        bestResult = video;
+        duration = {
+          youtube: videoDuration,
+          spotify: Number(songDuration),
+        };
+      }
+    }
+
+    res.json({
+      title: bestResult.title,
+      url: bestResult.url,
+      views: bestResult.views,
+      duration,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello, Express App!');
--- routes/spotify.js
+++ routes/spotify.js
@@ -1,87 +1,97 @@
-// const express = require("express");
-// const { searchSpotify, mmssToSeconds } = require("../utils/spotify");
+const express = require("express");
+const { searchSpotify, mmssToSeconds } = require("../utils/spotify");
+const { getColorFromURL } = require("color-thief-node");
 
-// const router = express.Router();
+const router = express.Router();
 
-// require("dotenv").config();
-// router.get("/search/:query", async (req, res) => {
-//   const { query } = req.params;
+require("dotenv").config();
+router.get("/search/:query", async (req, res) => {
+  const { query } = req.params;
 
-//   try {
-//     const spotifyResults = await searchSpotify(query);
+  try {
+    const spotifyResults = await searchSpotify(query);
 
-//     const formatedSpotifyResults = spotifyResults.map((item) => ({
-//       title: item.name,
-//       artist: item.artists[0].name,
-//       allArtists: item.artists.map((artist) => artist.name),
-//       spotifyUrl: item.external_urls.spotify,
-//       id: item.id,
-//       thumbnail: item.album.images[0].url,
-//       thumbnailSmall: item.album.images.slice(-1)[0]?.url,
-//       duration: item.duration_ms,
-//       popularity: item.popularity,
-//       type: "track",
-//     }));
-//     console.log(spotifyResults);
+    const formatedSpotifyResults = await Promise.all(
+      spotifyResults.map(async (item) => {
+        const dominantColor = await getColorFromURL(item.album.images[0].url);
+        const color = dominantColor
+          .map((c) => c.toString(16).padStart(2, "0"))
+          .join("");
 
-//     res.json(formatedSpotifyResults);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res
-//       .status(500)
-//       .json({ error: "An error occurred while processing your request." });
-//   }
-// });
+        return {
+          title: item.name,
+          artist: item.artists[0].name,
+          allArtists: item.artists.map((artist) => artist.name),
+          spotifyUrl: item.external_urls.spotify,
+          id: item.id,
+          thumbnail: item.album.images[0].url,
+          thumbnailSmall: item.album.images.slice(-1)[0]?.url,
+          duration: item.duration_ms,
+          popularity: item.popularity,
+          type: "track",
+          color: `#${color}`,
+        };
+      })
+    );
 
+    res.json(formatedSpotifyResults);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
+});
 
-// // get yt link from spotify song
+// get yt link from spotify song
 
-// // const yt = require("ytsr");
+const yt = require("ytsr");
 
-// // router.get("/search/:title/:artist/:time", async (req, res) => {
-// //   const { title, artist, time } = req.params;
+router.get("/search/:title/:artist/:time", async (req, res) => {
+  const { title, artist, time } = req.params;
 
-// //   try {
-// //     const filters = await yt.getFilters(`${title} ${artist}`);
-// //     const filter = filters.get("Type").get("Video");
+  try {
+    // const filters = await yt.getFilters(`${title} ${artist}`);
+    console.log(`${title} ${artist}`)
+    // const filter = await filters.get("Type").get("Video");
 
-// //     const searchOptions = {
-// //       limit: 10,
-// //     };
+    const searchOptions = {
+      limit: 10,
+    };
 
-// //     const searchResults = await yt(filter.url, searchOptions);
+    const searchResults = await yt(`${title} ${artist}`, searchOptions);
 
-// //     let bestResult = null;
-// //     let minTimeDiff = Infinity;
-// //     let durations = {};
+    let bestResult = null;
+    let minTimeDiff = Infinity;
+    let duration = {};
 
-// //     for (const video of searchResults.items) {
-// //       const songDuration = time;
-// //       const videoDuration = mmssToSeconds(video.duration);
-// //       const timeDiff = Math.abs(videoDuration - songDuration);
+    for (const video of searchResults.items) {
+      const songDuration = time;
+      const videoDuration = mmssToSeconds(video.duration);
+      const timeDiff = Math.abs(videoDuration - songDuration);
 
-// //       if (timeDiff < minTimeDiff) {
-// //         minTimeDiff = timeDiff;
-// //         bestResult = video;
-// //         duration = {
-// //           youtube: videoDuration,
-// //           spotify:  Number(songDuration),
-// //         };
-// //       }
-// //     }
+      if (timeDiff < minTimeDiff) {
+        minTimeDiff = timeDiff;
+        bestResult = video;
+        duration = {
+          youtube: videoDuration,
+          spotify: Number(songDuration),
+        };
+      }
+    }
 
-// //     res.json({
-// //       title: bestResult.title,
-// //       url: bestResult.url,
-// //       views: bestResult.views,
-// //       duration,
-// //     });
-// //   } catch (error) {
-// //     console.error("Error:", error);
-// //     res
-// //       .status(500)
-// //       .json({ error: "An error occurred while processing your request." });
-// //   }
-// // });
+    res.json({
+      title: bestResult.title,
+      url: bestResult.url,
+      views: bestResult.views,
+      duration,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing your request." });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
